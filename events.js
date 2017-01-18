ccac94b9e136e5f54bcf47448ef5a489a31b41ae
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-01-18 13:12:37 -0600

    Clicking on sections task complete

--- events.js
+++ events.js
@@ -1,1 +1,9 @@
 // Clicking on sections
+document.addEventListener('click', clickOutput, true);
+
+
+function clickOutput(element) {
+    clickedElement = window.event.target.innerText;
+    console.log(clickedElement);
+    document.getElementById("output-target").innerHTML = "You clicked on the " + clickedElement + " section";
+}
